d7ee6cd7253cf5bbc8bbd6979b4c344ea65ba7f8
Antonio Karam <antonio-karam@outlook.com>
2024-09-30 22:08:00 -0400

    pluse width mod

--- controller.py
+++ controller.py
@@ -6,7 +6,7 @@ from time import sleep
 factory = PiGPIOFactory()
 
 # Use hardware PWM pin (e.g., GPIO 18)
-servo = Servo(18, pin_factory=factory, min_pulse_width=0.0001, max_pulse_width=0.004)
+servo = Servo(18, pin_factory=factory, min_pulse_width=0.0001, max_pulse_width=0.003)
 
 try:
     while True:
