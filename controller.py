bd249cf855826933fce0bf6227840d593d02a749
Antonio Karam <antonio-karam@outlook.com>
2024-09-30 21:55:34 -0400

    new pin factory

--- controller.py
+++ controller.py
@@ -1,6 +1,9 @@
+from gpiozero.pins.pigpio import PiGPIOFactory
+from gpiozero import Device
 from gpiozero import Servo
 from time import sleep
 
+Servo.pin_factory = PiGPIOFactory()
 servo = Servo(25)
 
 try:
