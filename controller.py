f24e65433fcecebffb768b91156390af46bda7ed
Antonio Karam <antonio-karam@outlook.com>
2024-09-30 21:34:22 -0400

    duty cycle v7

--- controller.py
+++ controller.py
@@ -11,17 +11,42 @@ pwm = GPIO.PWM(servo_pin, 50)
 pwm.start(0)
 
 try:
-    # Slowly increase the angle (0 to 180 degrees)
-    for duty in range(0, 110):  # Duty cycle from 2.5% to 12.5%
-        pwm.ChangeDutyCycle(duty / 10.0)  # Convert to percentage
-        time.sleep(0.1)  # Small delay between steps
-    time.sleep(1)
+    # # Slowly increase the angle (0 to 180 degrees)
+    # for duty in range(0, 110):  # Duty cycle from 2.5% to 12.5%
+    #     pwm.ChangeDutyCycle(duty / 10.0)  # Convert to percentage
+    #     time.sleep(0.1)  # Small delay between steps
+    # time.sleep(1)
 
-    # Slowly decrease the angle (180 to 0 degrees)
-    for duty in range(110, 0, -1):
-        pwm.ChangeDutyCycle(duty / 10.0)
-        time.sleep(0.1)
+    # # Slowly decrease the angle (180 to 0 degrees)
+    # for duty in range(110, 0, -1):
+    #     pwm.ChangeDutyCycle(duty / 10.0)
+    #     time.sleep(0.1)
+    # time.sleep(1)
+    
+    pwm.ChangeDutyCycle(0)
     time.sleep(1)
+    
+    pwm.ChangeDutyCycle(0.5)
+    time.sleep(1)
+    
+    pwm.ChangeDutyCycle(1)
+    time.sleep(1)
+    
+    pwm.ChangeDutyCycle(5)
+    time.sleep(1)
+    
+    pwm.ChangeDutyCycle(10)
+    time.sleep(1)
+    
+    pwm.ChangeDutyCycle(100)
+    time.sleep(1)
+    
+    pwm.ChangeDutyCycle(-5)
+    time.sleep(1)
+    
+    pwm.ChangeDutyCycle(-1)
+    time.sleep(1)
+    
 
 finally:
     pwm.stop()
