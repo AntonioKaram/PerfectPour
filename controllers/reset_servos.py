89b34a676021ec1e30f9a181aacd86ce27ca7340
Antonio Karam <antonio-karam@outlook.com>
2024-10-29 22:51:33 -0400

    changes in linear motor workings

--- controllers/reset_servos.py
+++ controllers/reset_servos.py
@@ -1,20 +1,30 @@
 
 from gpiozero import Servo
 from gpiozero.pins.pigpio import PiGPIOFactory
+import RPi.GPIO as GPIO
+from time import sleep
 
-# Create a PiGPIOFactory instance
-factory = PiGPIOFactory()
+GPIO.setwarnings(False)
 
-min_pulse_width = 0.00008  # 0 degrees
-max_pulse_width = 0.0023  # 180 degrees
+in1 = 17
+in2 = 27
 
-# Use Raspberry Pi v4 PWM pins
-servo1 = Servo(12, pin_factory=factory, min_pulse_width=0.5/1000, max_pulse_width=2.5/1000)
-servo2 = Servo(13, pin_factory=factory, min_pulse_width=0.5/1000, max_pulse_width=2.5/1000)
+GPIO.setmode(GPIO.BCM)
 
-top_linear = Servo(17, pin_factory=factory)
-bottom_linear = Servo(27, pin_factory=factory)
+GPIO.setup(in1, GPIO.OUT)
+GPIO.setup(in2, GPIO.OUT)
 
+print("setup..")
+GPIO.output(in1, GPIO.LOW)
+GPIO.output(in2, GPIO.LOW)
+sleep(2)
 
-top_linear.min()
-bottom_linear.min()
+print("try1...")
+GPIO.output(in1, GPIO.LOW)
+GPIO.output(in2, GPIO.HIGH)
+sleep(2)
+
+print("try2...")
+GPIO.output(in1, GPIO.HIGH)
+GPIO.output(in2, GPIO.LOW)
+sleep(2)
