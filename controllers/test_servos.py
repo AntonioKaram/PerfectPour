63a0931760b9a487119aa4b175fc2f8bf96bcfa8
Antonio Karam <antonio-karam@outlook.com>
2024-11-25 18:52:36 -0500

    measuring script

--- controllers/test_servos.py
+++ controllers/test_servos.py
@@ -17,7 +17,7 @@ max_pulse_width = 0.0023  # 180 degrees
 
 # Set up rotating servos
 servo1 = Servo(12, pin_factory=factory, min_pulse_width=min_pulse_width, max_pulse_width=max_pulse_width, initial_value=None)
-#servo2 = Servo(13, pin_factory=factory, min_pulse_width=min_pulse_width, max_pulse_width=max_pulse_width, initial_value=None)
+servo2 = Servo(13, pin_factory=factory, min_pulse_width=min_pulse_width, max_pulse_width=max_pulse_width, initial_value=None)
 
 # Set up linear servos
 bottom_low = 22
