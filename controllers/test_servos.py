4e4b86540c22980269709fd41ac0f0eadb2bdbee
Antonio Karam <antonio-karam@outlook.com>
2024-11-25 19:51:23 -0500

    measuring script post burnout

--- controllers/test_servos.py
+++ controllers/test_servos.py
@@ -116,15 +116,11 @@ def rot():
 
     
 def reset():
-    rotate_servo(servo2, 0, 0.3, 0.01)
-    
-    #sleep(1)
-    #rotate_servo(servo2, 0.3, 0, 0.01)
-    # rotate_servo(servo2, 0, 0.15, 0.1)
-    # sleep(1)
-    # rotate_servo(servo2, 0.15, 0.3, 0.01)
-    # sleep(1)
-    # rotate_servo(servo2, 0.3, 0, 0.01)
+    rotate_servo(servo2, 0, 0.15, 0.1)
+    sleep(1)
+    rotate_servo(servo2, 0.15, 0.3, 0.01)
+    sleep(1)
+    rotate_servo(servo2, 0.3, 0, 0.01)
     
     
 
