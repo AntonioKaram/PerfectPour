0453bb8d878abc7313c2d4277908f4b49f0cf0e9
Antonio Karam <antonio-karam@outlook.com>
2024-11-25 19:05:54 -0500

    measuring script 2

--- controllers/test_servos.py
+++ controllers/test_servos.py
@@ -118,7 +118,7 @@ def rot():
     
 def reset():
     #rotate_servo(servo2, 0, 0.3, 0.01)
-    rotate_servo(servo1, 0.3, 0, 0.01)
+    rotate_servo(servo2, 0.3, 0, 0.01)
     
     #sleep(1)
     #rotate_servo(servo2, 0.3, 0, 0.01)
