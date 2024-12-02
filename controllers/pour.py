91c1a80897a6b610386d692e5ab7d59039a65b23
Antonio Karam <antonio-karam@outlook.com>
2024-12-02 01:32:57 -0500

    pulse width 29

--- controllers/pour.py
+++ controllers/pour.py
@@ -179,10 +179,12 @@ def main():
     # # Wait for both threads to finish
     # # thread1.join()
     # thread2.join()
-    smooth_move_servo(13, 1, 1,{
-    'MIN': 800,
-    'MAX': 2300,
-    'CENTER': 1190})
+    # Set the servo to position 1 and keep it there
+    target_pulse = calculate_pulse_width(1, {
+        'MIN': 800,
+        'MAX': 2300,
+        'CENTER': 1190})
+    pi.set_servo_pulsewidth(13, target_pulse)
     
     sleep(5)
     
