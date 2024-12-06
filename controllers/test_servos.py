51a50312930e3cf7bca141a66eae4bcc261fcb3e
Antonio Karam <antonio-karam@outlook.com>
2024-12-06 16:27:50 -0500

    linears23

--- controllers/test_servos.py
+++ controllers/test_servos.py
@@ -142,7 +142,7 @@ try:
 
         if choice == 'r':
             # control_rotational_servos()
-            GPIO_move(backgate_high, backgate_low, 5)
+            GPIO_move(backgate_high, backgate_low, 3)
             
         elif choice == 'reset':
             reset_servos()
